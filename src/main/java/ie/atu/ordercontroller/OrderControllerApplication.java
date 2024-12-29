6174b441d615a3cb0b878d55a5d6a82c0d207db0
Rory O Loughlin <g00422319@atu.ie>
2024-12-29 16:33:10 +0000

    committing project

---
+++ src/main/java/ie/atu/ordercontroller/OrderControllerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.ordercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/ordercontroller/OrderControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.ordercontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
